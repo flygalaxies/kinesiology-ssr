cf75dcfe98b66b128e3270c9194af0144d215233
unknown <iantolmay@mweb.co.za>
2018-01-18 15:59:29 +0200

    updated prices and added search

--- src/app/main/prices/prices.component.ts
+++ src/app/main/prices/prices.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { Meta, Title } from '@angular/platform-browser'
 
 @Component({
@@ -8,7 +9,7 @@ import { Meta, Title } from '@angular/platform-browser'
 })
 export class PricesComponent implements OnInit {
 
-  constructor(meta: Meta, title: Title) {
+  constructor(meta: Meta, title: Title, @Inject(PLATFORM_ID) private platformId: Object) {
     title.setTitle('Kinesiology Session Rates- The time and cost of each type of session');
     meta.addTags([
       { name: 'author', content: 'kinesiologyPractice.co.za' },
@@ -20,4 +21,24 @@ export class PricesComponent implements OnInit {
   ngOnInit() {
   }
 
+  priceSearch() {
+    if (isPlatformBrowser(this.platformId)) {
+      let input, filter, table, tbody;
+      input = document.getElementById("priceInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("priceTable");
+      tbody = table.getElementsByTagName("tbody");
+      for (let i = 0; i < tbody.length; i++) {
+        let td = tbody[i].getElementsByTagName("td")[0];
+        //let id = document.getElementById("therapy");
+        if (td) {
+          if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            tbody[i].style.display = "";
+          } else {
+            tbody[i].style.display = "none";
+          }
+        }
+      }
+    }
+  }
 }
